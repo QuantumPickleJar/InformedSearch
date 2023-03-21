48ab9e6a46c623898b11a6001d821ff5e287644f
Vince <vmorrill@appliedsystems.com>
2023-03-21 13:49:51 -0500

    Improved the readability of the result function, still cannot resolve TypeError on expand

--- A1/rush_hour.py
+++ A1/rush_hour.py
@@ -168,7 +168,7 @@ class RushHour(Problem):
 
     def goal_test(self, state):  
         #is the red car EXACTLY where it needs to be on the grid, in the right orientation?
-        if state.cars[0].left == 2 and state.cars[0].top == 6:
+        if state.cars[0].left == 2 and state.cars[0].top == 5:
             return True
         else:
             return False
@@ -176,50 +176,72 @@ class RushHour(Problem):
     # must advance to the next state based on the action taken from the current state
     def result(self, state, action):
         # parse the top and left modification from the action
-        car_id, move = action 
 
-        # result_grid = [[state.grid[i][j] for j in range(len(state.grid[0]))]\
-        #                for i in range(len(state.grid))]    
-    
+        # apply the move to the car based on the action
+        car_id, dx, dy = action
         result_grid = [row[:] for row in state.grid]
 
         car = state.cars[car_id]
+        top, left, L, orientation = car.top + dy, car.left + dx, car.L, car.orientation
 
-        # apply the move to the car based on the action
-            # if the action calls for this car to move:
+        # first, update empty spaces to reflect the move
+        if orientation:
+            for j in range(left, left + L):
+                result_grid[top][j] = -1
+        else:
+            for i in range(top, top + L):
+                result_grid[i][left] = -1
 
+        # for i in range(top, top + L):
+        #     for j in range (left, left + L):
+        #         result_grid[i][j] = -1
+
+        # send new coordainates 
+        if orientation:
+            for j in range(left + dx, left + L + dx):
+                result_grid[top + dy][j] = id
+        else:
+            for i in range(top + dy, top + L + dy):
+                # result_grid[i][left + dx] = id
+                result_grid[i][left] = id
+
+
+
+
+        # for i in range(top, top + L):
+        #     for j in range(left, left + L):
+        #         result_grid[i][j] = car_id
+        '''
+        car = state.cars[car_id]
+
+        # if the action calls for this car to move:
         if car.orientation: 
-            if move == 'left':
-                result_grid[car.top][car.left - 1] = car.id
-                # update empty cell
-                
-                result_grid[car.top][car.left + car.L - 1] = -1     
-                # update the car's property to reflect the move
-                car.left -= 1
+        if move == 'left':
+            result_grid[car.top][car.left - 1] = car.id
+            # update empty cell
+            
+            result_grid[car.top][car.left + car.L - 1] = -1     
+            # update the car's property to reflect the move
+            car.left -= 1
 
-            else: # move = right 
-                result_grid[car.top][car.left + car.L] = car.id
-                result_grid[car.top][car.left] = -1     
-                # update the car's property to reflect the move
-                car.left += 1
+        else: # move = right 
+            result_grid[car.top][car.left + car.L] = car.id
+            result_grid[car.top][car.left] = -1     
+            # update the car's property to reflect the move
+            car.left += 1
         else: 
-            if move == 'up':
-                result_grid[car.top - 1][car.left] = car.id
-                result_grid[car.top + car.L - 1][car.left] = -1     
-                car.left -= 1
+        if move == 'up':
+            result_grid[car.top - 1][car.left] = car.id
+            result_grid[car.top + car.L - 1][car.left] = -1     
+            car.left -= 1
 
-            else:  # move = down
-                result_grid[car.top + car.L][car.left] = car.id
-                result_grid[car.top][car.left] = -1     
-                # update the car's property to reflect the move
-                car.top += 1
-
-            # if car == action[0]:
-            #     new_car = Car(car.id, car.top + action[2],  \
-            #                     car.left + action[1], car.L, car.orientation)
-            #     updated_cars.append(new_car)
-            # else:   # otherwise, we can just pass it right back in 
-            #     updated_cars.append(car)
+        else:  # move = down
+            result_grid[car.top + car.L][car.left] = car.id
+            result_grid[car.top][car.left] = -1     
+            # update the car's property to reflect the move
+            car.top += 1
+        '''
+ 
 
         # initalize the problem with the new grid
         
@@ -232,10 +254,12 @@ class RushHour(Problem):
         Heuristic that is admissible because it never overestimates distance to goal;
         other cars can only block and do not reduce the distance to the goal.
     '''
+    
     def h(self, node):
         car_red = node.state.cars[0]
         dist = len(node.state.grid) - car_red.left - car_red.L
         return dist
+    
         # alternate heuristic ideas:
         # what if we weighted vehicles by their L and use this to calcualte a weighted path to goal position?
         if (self.heuristic==0):
@@ -281,7 +305,7 @@ def main():
     for i in state.cars:
         print(i)
 
-    problem = RushHour(state)
+    problem = RushHour(state,0)
     
     start = timer()
     goal = astar_search(problem)
